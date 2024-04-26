9701c0e88dd3f46cb6bcbbc5a150e7998094a8f3
sayemhossin <sayemhossin22@gmail.com>
2024-04-26 22:47:11 +0600

    hide firebase key

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC06GP2VfkoGD5pWIYhoVwyU9GeZxJJL-g",
-  authDomain: "assignment-ten-3d34f.firebaseapp.com",
-  projectId: "assignment-ten-3d34f",
-  storageBucket: "assignment-ten-3d34f.appspot.com",
-  messagingSenderId: "636167046639",
-  appId: "1:636167046639:web:e0e85ccfddf388b7f1221a"
+  apiKey: import.meta.env.VITE_APIKEY ,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN ,
+  projectId: import.meta.env.VITE_PROJECTID ,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET ,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID ,
+  appId: import.meta.env.VITE_APPID 
 };
 
 // Initialize Firebase
